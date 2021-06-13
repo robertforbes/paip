90278e2dcbd8dc837d8127146280756ae9354a91
Robert Forbes <robert.forbes@pentlandedge.com>
2021-06-13 23:13:56 +0100

    Made the improved first-names function the default.

--- ch1/ch1.lisp
+++ ch1/ch1.lisp
@@ -1,18 +1,3 @@
-(defun last-name (name)
-  "Select the last name from a name represented as a list."
-  (first (last name)))
-
-(defun first-name (name)
-  "Select the first name from a name represented as a list."
-  (first name))
-
-(defun test-names ()
-  '((John Q Public) (Malcolm X) (Admiral Grace Murray Hopper) (Spot)
-    (Aristotle) (A A Milne) (Z Z Top) (Sir Larry Olivier) (Miss Scarlet)))
-
-(defun first-names (names)
-  (mapcar #' first-name names))
-
 (defparameter *titles*
   '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General)
   "A list of titles that can appear at the start of a name.")
@@ -21,8 +6,25 @@
   '(MD Jr)
   "A list of qualifications that can appear at the end of a name.")
 
-(defun first-name2 (name)
+(defun last-name (name)
+  "Select the last name from a name represented as a list."
+  (first (last name)))
+
+;(defun first-name (name)
+;  "Select the first name from a name represented as a list."
+;  (first name))
+
+(defun first-name (name)
   "Select the first name from a name  represented as a list. Skip titles."
   (if (member (first name) *titles*)
     (first-name2 (rest name))
     (first name)))
+
+(defun test-names ()
+  '((John Q Public) (Malcolm X) (Admiral Grace Murray Hopper) (Spot)
+    (Aristotle) (A A Milne) (Z Z Top) (Sir Larry Olivier) (Miss Scarlet)))
+
+(defun first-names (names)
+  (mapcar #' first-name names))
+
+
