72c63c88d28cf84cd004b9f45b9cca625207f1d7
Robert Forbes <robert.forbes@pentlandedge.com>
2021-06-13 23:08:39 +0100

    Added an improved first-name function, skipping titles.

--- ch1/ch1.lisp
+++ ch1/ch1.lisp
@@ -12,3 +12,17 @@
 
 (defun first-names (names)
   (mapcar #' first-name names))
+
+(defparameter *titles*
+  '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General)
+  "A list of titles that can appear at the start of a name.")
+
+(defparameter *quals*
+  '(MD Jr)
+  "A list of qualifications that can appear at the end of a name.")
+
+(defun first-name2 (name)
+  "Select the first name from a name  represented as a list. Skip titles."
+  (if (member (first name) *titles*)
+    (first-name2 (rest name))
+    (first name)))
