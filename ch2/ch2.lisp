4d67a9e94c574aa63eca9b5cee45e4842029c53b
Robert Forbes <robert.forbes@pentlandedge.com>
2022-05-31 22:48:53 +0100

    Re-ordered the functions to avoid a style warning.

--- ch2/ch2.lisp
+++ ch2/ch2.lisp
@@ -1,10 +1,10 @@
-(defun one-of (set)
-  "Pick one element of set and make a list of it."
-  (list (random-elt set)))
-
 (defun random-elt (choices)
   "Choose an element from a list at random."
   (elt choices (random (length choices))))
+
+(defun one-of (set)
+  "Pick one element of set and make a list of it."
+  (list (random-elt set)))
 
 (defun Article ()     (one-of '(the a)))
 (defun Noun ()        (one-of '(man ball woman table)))
