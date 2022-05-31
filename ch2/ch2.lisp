c7f8c9683c6a590d51ee5523691b816dd6bd1aca
Robert Forbes <robert.forbes@pentlandedge.com>
2022-05-31 22:57:29 +0100

    Added the first part of the grammar definition.

--- ch2/ch2.lisp
+++ ch2/ch2.lisp
@@ -14,3 +14,28 @@
 (defun verb-phrase () (append (Verb) (noun-phrase)))
 
 (defun sentence ()    (append (noun-phrase) (verb-phrase)))
+
+(defparameter *simple-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked))
+  "A grammar for a trivial subset of English.")
+
+(defvar *grammar* *simple-grammar*
+  "The grammar used by generate. Initially, this is *simple-grammar*, but we
+  can switch to other grammars.")
+
+(defun rule-lhs (rule)
+  "The left hand side of a rule."
+  (first rule))
+
+(defun rule-rhs (rule)
+  "The right hand side of a rule."
+  (rest (rest rule)))
+
+(defun rewrites (category)
+  "Return a list of the possible rewrites for this category."
+  (rule-rhs (assoc category *grammar*)))
