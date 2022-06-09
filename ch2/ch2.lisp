ad7c6838291b21d7cd376ffae9d2c7576dceee65
Robert Forbes <robert.forbes@pentlandedge.com>
2022-06-09 22:30:13 +0100

    Added code to generate the parse tree along with random sentences.

--- ch2/ch2.lisp
+++ ch2/ch2.lisp
@@ -106,3 +106,12 @@
 
 (setf *grammar* *bigger-grammar*)
 
+; Generate the tree structure used to output sentences.
+(defun generate-tree (phrase)
+  "Generate a random sentence or phrase, with a complete parse tree."
+  (cond ((listp phrase)
+         (mapcar #'generate-tree phrase))
+        ((rewrites phrase)
+         (cons phrase (generate-tree (random-elt (rewrites phrase)))))
+        (t (list phrase))))
+
