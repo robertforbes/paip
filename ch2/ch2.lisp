33ba7807125116d3a380bc3d93eb1752f1e2e195
Robert Forbes <robert.forbes@pentlandedge.com>
2022-06-09 22:09:34 +0100

    Added the bigger grammar.

--- ch2/ch2.lisp
+++ ch2/ch2.lisp
@@ -88,4 +88,21 @@
          (generate4 (random-elt (rewrites phrase))))
         (t (list phrase))))
 
+; Add an extended grammar.
+(defparameter *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+
+(setf *grammar* *bigger-grammar*)
 
