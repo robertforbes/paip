ce995d1704458789718ddb9e234f4a1e8c3f298c
Robert Forbes <robert.forbes@pentlandedge.com>
2022-06-10 23:14:43 +0100

    Added combine-all and generate-all

--- ch2/ch2.lisp
+++ ch2/ch2.lisp
@@ -104,7 +104,7 @@
     (Verb -> hit took saw liked)
     (Pronoun -> he she it these those that)))
 
-(setf *grammar* *bigger-grammar*)
+; (setf *grammar* *bigger-grammar*)
 
 ; Generate the tree structure used to output sentences.
 (defun generate-tree (phrase)
@@ -114,4 +114,22 @@
         ((rewrites phrase)
          (cons phrase (generate-tree (random-elt (rewrites phrase)))))
         (t (list phrase))))
+
+(defun combine-all (xlist ylist)
+  "Return a list of lists formed by appending a y to an x.
+  E.g., (combine-all '((a) (b)) '((1) (2)))
+  -> ((A 1) (B 2) (A 2) (B 2))."
+  (mappend #'(lambda (y)
+               (mapcar #'(lambda (x) (append x y)) xlist))
+           ylist))
+
+(defun generate-all (phrase)
+  "Generate a list of all possible expansions of this phrase."
+  (cond ((null phrase) (list nil))
+        ((listp phrase)
+         (combine-all (generate-all (first phrase))
+                      (generate-all (rest phrase))))
+        ((rewrites phrase)
+         (mappend #'generate-all (rewrites phrase)))
+        (t (list (list phrase)))))
 
