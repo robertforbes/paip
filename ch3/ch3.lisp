86a362b387378215356dc66e53a0a3cbc9f9a8d0
Robert Forbes <robert.forbes@pentlandedge.com>
2022-06-22 23:04:12 +0100

    Added a simple conditional example.

--- ch3/ch3.lisp
+++ ch3/ch3.lisp
@@ -9,3 +9,13 @@
 ; accessor function name-first, name-middle, and name-last.
 ; Create one in the REPL using:
 ; (setf b (make-name :first 'Barney :last 'Rubble))
+
+; Special forms for conditionals.
+
+(defun tax-bracket (income)
+  "Determine what percent tax should be paid for this income."
+  (cond ((< income 10000.00) 0.00)
+        ((< income 30000.00) 0.20)
+        ((< income 50000.00) 0.20)
+        ((< income 70000.00) 0.30)
+        (t                   0.35)))
